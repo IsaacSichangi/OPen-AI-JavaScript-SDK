603b7eaace7d5a779e1ce1be3a7aa911a515460a
Isaac Sichangi <isaacsichangi@dukapos.co.ke>
2023-05-02 02:48:27 +0300

    rearranged file structure

--- js/test.js
+++ js/test.js
@@ -1,9 +1,63 @@
-let openApi = new OpenApi("");
+let openApi = new OpenApi("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");
+/**
 openApi.listModels().then(function (value) {
 
     console.log(value);
 }, function (error) {
 
 });
+ **/
 
+/**
+openApi.retrieveModel(OpenApi.TEXT_DAVINCI_003).then(function (value) {
+
+    alert(JSON.stringify(value));
+
+}, function (error) {
+
+});
+ **/
+
+
+/**
+openApi.createCompletion(OpenApi.TEXT_DAVINCI_001, "capital of uganda is").then(function (value) {
+
+
+    alert(JSON.stringify(value));
+}, function (error) {
+
+});
+**/
+
+/**
+messages = [{role: "user", content: "Hello!"}];
+openApi.createChatCompletion(OpenApi.GPT_3_5_TURBO, messages).then(function (value) {
+
+
+    alert(JSON.stringify(value));
+}, function (error) {
+
+});
+**/
+
+/**
+openApi.createEdit(OpenApi.TEXT_DAVINCI_EDIT_001, "What day of the wek is it?", "Fix the spelling mistakes").then(function (value) {
+
+    alert(JSON.stringify(value));
+
+}, function (error) {
+
+})
+ **/
+
+openApi.createImage("nairobi in 2050").then(function (value) {
+
+    let url = value.data[0].url;
+
+    console.log(url);
+    alert(url);
+
+}, function (error) {
+
+})
 
